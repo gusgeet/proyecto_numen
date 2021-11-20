46285189fe2264017fb6c98313de7eab81232f6d
MichelVarela <73833649+MichelVarela@users.noreply.github.com>
2021-11-19 22:01:56 -0300

    centrado

--- proyectobeta/src/App.js
+++ proyectobeta/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Headers from "./components/Navbar/Header";
 import Main1 from "./components/Main-1";
 import Main2 from "./components/Main/Main-2";
+import Main3 from './components/Main/Main-3';
 import { GlobalStyles } from "./global-styles";
 import { FooterContainer } from "./container/footer"
 
@@ -15,6 +16,7 @@ function App() {
       <Navbar />
       <Main1 />
       <Main2 />
+      <Main3/>
       <>
       <GlobalStyles />
       <FooterContainer/>
